c97c83418c65fa5326e3071dc8a4bfc515573f23
Josh Hill <joshuahill84@gmail.com>
2015-12-15 14:55:10 -0600

    first build

---
+++ week9Day2homework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="week9Day2homework.MvcApplication" Language="C#" %>
---
+++ week9Day2homework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(week9Day2homework.Startup))]
+namespace week9Day2homework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
